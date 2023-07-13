9bbd8ecf19cb972e75b949a21e8073ac27623f20
Mariem <mariem.ehab@ieee.org>
2023-07-13 02:17:50 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints exactly with proper grammar, but
+ * the outcome is a piece of art,, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+	return (0)
+}
