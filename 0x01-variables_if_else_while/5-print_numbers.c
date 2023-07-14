d29966cd00043b334cc4d8eb1098e28edcd5d8cc
Mariem <mariem.ehab@ieee.org>
2023-07-14 03:31:42 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0-9
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i++);
+	}
+	putchar('\n');
+
+	return (0);
+}
