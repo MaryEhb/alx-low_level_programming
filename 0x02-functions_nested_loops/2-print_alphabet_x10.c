9588ca161f65dda014d9d38cf7a470086bc6ff50
Mariem <mariem.ehab@ieee.org>
2023-07-18 04:53:36 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		int i = 97;
+
+		while (i < 123)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
