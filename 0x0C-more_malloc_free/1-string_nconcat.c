1692465f5bea2de943e594e9392433933b9ccc0a
Mariem <mariem.ehab@ieee.org>
2023-08-10 14:33:41 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size2++;
 	}
 
-	p = malloc(sizeof(p) * size1 + n + 1);
+	p = malloc(sizeof(p) * (size1 + n + 1));
 
 	if (!p)
 		return (0);
