363447dbdacce5da745e0255c95ccc1a3cd3ecd7
Mariem <mariem.ehab@ieee.org>
2023-08-10 14:06:03 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * string_nconcat -  concatenates two strings.
+ * @s1: char
+ * @s2: char
+ * @n: int
+ * Return: pointer shall point to a newly allocated space in memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: 
+ * @size:
+ * Return: 
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
