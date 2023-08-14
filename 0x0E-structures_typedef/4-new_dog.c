92581fa919bbf8b041ac8d01e648b37b2dc0fa01
Mariem <mariem.ehab@ieee.org>
2023-08-14 07:19:41 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,48 @@
 #include "dog.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: first character in string pointer
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * _strcpy -  copies the string pointed to by src, including
+ * the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: buffer pointer
+ * @src: string pointer
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - create a dog
  * @name: name
  * @age: age
@@ -22,7 +64,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!dog)
 		return (0);
 
-	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 
 	if (!dog->name)
 	{
@@ -30,7 +72,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (0);
 	}
 
-	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 
 	if (!dog->owner)
 	{
@@ -38,9 +80,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (0);
 	}
 
-	dog->name = strcpy(dog->name, name);
+	dog->name = _strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = strcpy(dog->owner, owner);
+	dog->owner = _strcpy(dog->owner, owner);
 
 	return (dog);
 }
