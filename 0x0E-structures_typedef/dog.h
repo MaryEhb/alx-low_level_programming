1cef78095c6f6aa235c38b9fa6ed26686791f261
Mariem <mariem.ehab@ieee.org>
2023-08-14 05:35:28 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog - dog
+ * @name: name
+ * @age: age
+ * @owner: own
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
