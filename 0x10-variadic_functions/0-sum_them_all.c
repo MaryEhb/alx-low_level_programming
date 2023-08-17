716652f59b3035f6388a39b9ce27641128a87008
Mariem <mariem.ehab@ieee.org>
2023-08-17 08:51:24 -0700

    main files

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -  returns the sum of all its parameters.
+ * @n: num of parameters
+ * Return: sum of all its parameters, If n == 0, return 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
