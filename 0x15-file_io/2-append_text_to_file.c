9249659cf90b9c8312ba30e6243030d4cfd49301
Mariem <mariem.ehab@ieee.org>
2023-09-04 09:50:32 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,3 +6,26 @@
  * @text_content: string
  * Return: 1 on success and -1 on failure
  */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	if (!text_content || !*text_content)
+		return (1);
+
+	while (text_content[len])
+		len++;
+
+	write(file, text_content, len);
+	return (1);
+}
+
