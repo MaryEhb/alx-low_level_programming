0c7e205163160925fa9eb78744a9c711a884ba15
Mariem <mariem.ehab@ieee.org>
2023-10-12 13:31:11 -0700

    8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,6 +10,28 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
+	unsigned int i = 0;
 
-	if ()
+	if (!head || !*head)
+		return (-1);
+
+	while (i != index && tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (!tmp)
+		return (-1);
+
+	if (tmp->prev)
+		tmp->prev->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+
+	if (i == 0)
+		*head = tmp->next;
+
+	free(tmp);
+
+	return (1);
 }
