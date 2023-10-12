2d9be24ce7a979eefeb320b774029054183401e9
Mariem <mariem.ehab@ieee.org>
2023-10-12 13:12:21 -0700

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *tmp = *h;
 	unsigned int count = 0;
 
-	if (!tmp)
+	if (!tmp && idx > 0)
 		return (NULL);
 
 	if (idx == 0)
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,5 +11,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
 
-	
+	if ()
 }
